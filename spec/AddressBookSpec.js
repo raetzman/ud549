695ca7a85acb66326a928cd9cd53c9d7bcc7a7e7
raetzman <raetzman@gmx.de>
2018-06-06 00:24:05 +0200

    finished introduction

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,4 +1,12 @@
 describe("Address Book", function(){
+
+	var addressBook, thisContact;
+
+	beforeEach(function(){
+		addressBook = new AddressBook();
+		thisContact = new Contact();
+	});
+
 	it("Matthias should be able to add a contact", function(){
 		var addressBook = new AddressBook();
 		var thisContact = new Contact();
@@ -18,4 +26,23 @@ describe("Address Book", function(){
 		expect(addressBook.getContact(0)).not.toBeDefined();
 	}
 	);
+});
+
+describe("Async Address Book", function(){
+	var addressBook = new AddressBook();
+
+	beforeEach(function(done){
+		addressBook.getInitialContacts(function(){
+			done();
+		});
+	});
+
+	it('should grab an initial contacts', 
+	function(done){
+		
+		addressBook.getInitialContacts();
+		expect(addressBook.initialComplete).toBe(true);
+		done();
+	}
+);
 });
